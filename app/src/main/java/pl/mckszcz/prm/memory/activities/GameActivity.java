e5bb643d1d4a31e1cf5fdcf5135ba150a054ac5a
mckszcz <37812677+mckszcz@users.noreply.github.com>
2019-03-27 01:36:37 +0100

    added board creation

--- app/src/main/java/pl/mckszcz/prm/memory/activities/GameActivity.java
+++ app/src/main/java/pl/mckszcz/prm/memory/activities/GameActivity.java
@@ -2,39 +2,109 @@ package pl.mckszcz.prm.memory.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.widget.TableLayout;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.collect.Lists;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import lombok.Getter;
+import lombok.Setter;
 import pl.mckszcz.prm.memory.R;
+import pl.mckszcz.prm.memory.model.Card;
+import pl.mckszcz.prm.memory.model.Row;
 
+@Getter
+@Setter
 public class GameActivity extends AppCompatActivity {
 
-    Map<CardType, ImageView> cardMap;
+    private List<Card> cardList;
+    private List<Row> rowList;
+    private Card selectedCard;
+    private Card cardOne;
+    private Card cardTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        this.cardMap = new HashMap<>();
+        createBoard(6);
     }
 
-    private void createBoard() {
-
+    private void createBoard(int pairNumber) {
+        TableLayout layout = findViewById(R.id.tableLayout);
+        createRows(pairNumber);
+        createCards(pairNumber);
+        fillRows();
+        rowList.forEach(layout::addView);
+        addListeners();
     }
 
-    private void gameLoop() {
-
-    }
-
-    protected enum CardType {
-
-        BANG, CIRCLE, CROSS, FOUR_STAR, HEART, PENTAGON;
-
-        public CardType getType() {
-            return this;
+    private void createCards(int pairNumber) {
+        cardList = new ArrayList<>();
+        for (int i = 0; i < pairNumber; i++) {
+            Card cardOne = new Card(this);
+            cardOne.setCardType(Card.CardType.valueOf(i));
+            Card cardTwo = new Card(this);
+            cardTwo.setCardType(Card.CardType.valueOf(i));
+            cardList.add(cardOne);
+            cardList.add(cardTwo);
         }
+        Collections.shuffle(cardList);
+    }
+
+    private void createRows(int pairNumber) {
+        rowList = new ArrayList<>();
+        if (pairNumber < 3) {
+            rowList.add(new Row(this));
+            rowList.add(new Row(this));
+        } else {
+            rowList.add(new Row(this));
+            rowList.add(new Row(this));
+            rowList.add(new Row(this));
+        }
+    }
+
+    private void fillRows() {
+        int cardsInRow = Math.round(cardList.size() / rowList.size());
+        List<List<Card>> list = Lists.partition(cardList, cardsInRow);
+        for (int i = 0; i < rowList.size(); i++) {
+            rowList.get(i).addToRow(list.get(i));
+        }
+    }
+
+    private void addListeners() {
+        cardList.forEach(card -> {
+            card.setOnClickListener(l -> {
+                selectedCard = card;
+                card.setImageResource(card.getImage(card.getCardType()));
+                Timer t = new Timer();
+                t.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (cardOne == null && cardTwo == null) {
+                            cardOne = selectedCard;
+                            System.out.println("Card one: " + cardOne.getCardType());
+                        } else if (cardOne != null && cardOne != selectedCard && cardTwo == null) {
+                            cardTwo = selectedCard;
+                            System.out.println("Card two: " + cardTwo.getCardType());
+                            if (cardOne.getCardType() == cardTwo.getCardType()) {
+                                card.setMatched(true);
+                                System.out.println(card.getCardType() + " " + card.isMatched());
+                            }
+                        }
+                        if (!card.isMatched())
+                            card.setImageResource(R.drawable.unknown);
+                        cardOne = null;
+                        cardTwo = null;
+                    }
+                }, 2000);
+            });
+        });
     }
 
 }
--- app/src/main/java/pl/mckszcz/prm/memory/model/Card.java
+++ app/src/main/java/pl/mckszcz/prm/memory/model/Card.java
@@ -2,16 +2,75 @@ package pl.mckszcz.prm.memory.model;
 
 import android.content.Context;
 import android.support.v7.widget.AppCompatImageView;
+import android.widget.TableRow;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import lombok.Getter;
 import lombok.Setter;
+import pl.mckszcz.prm.memory.R;
 
 @Getter
 @Setter
 public class Card extends AppCompatImageView {
 
+    private CardType cardType;
+    private boolean isMatched = false;
+
     public Card(Context context) {
         super(context);
+        TableRow.LayoutParams params = new TableRow.LayoutParams();
+        params.width = 0;
+        params.height = TableRow.LayoutParams.WRAP_CONTENT;
+        params.weight = 1;
+        setAdjustViewBounds(true);
+        setPadding(2, 0, 2, 2);
+        setScaleType(ScaleType.FIT_XY);
+        setLayoutParams(params);
+        setImageResource(R.drawable.unknown);
+    }
+
+    public int getImage(CardType cardType) {
+        switch (cardType) {
+            case BANG:
+                return R.drawable.bang1;
+            case CIRCLE:
+                return R.drawable.circle1;
+            case CROSS:
+                return R.drawable.cross1;
+            case FOUR_STAR:
+                return R.drawable.four_star1;
+            case HEART:
+                return R.drawable.heart1;
+            case PENTAGON:
+                return R.drawable.pentagon1;
+        }
+        return R.drawable.unknown;
+    }
+
+    public enum CardType {
+
+        BANG(0), CIRCLE(1), CROSS(2), FOUR_STAR(3), HEART(4), PENTAGON(5);
+
+        private static Map map = new HashMap<>();
+
+        static {
+            for (CardType cardType : CardType.values()) {
+                map.put(cardType.value, cardType);
+            }
+        }
+
+        private int value;
+
+        CardType(int value) {
+            this.value = value;
+        }
+
+        public static CardType valueOf(int cardType) {
+            return (CardType) map.get(cardType);
+        }
+
     }
 
 }
